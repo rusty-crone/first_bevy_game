e54d430d2607c68354dd1faa58c3434970ecb6eb
Todd W Crone <tcrone@newrelic.com>
2024-01-26 14:19:38 -0500

    End of current Bevy Book

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
 use bevy::prelude::*;
 
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_systems(Startup, add_people)
+            .add_systems(Update, (update_people, greet_people).chain());
+    }
+}
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
 #[derive(Component)]
 struct Person;
 
 #[derive(Component)]
 struct Name(String);
-
-fn hello_world() {
-    println!("Hello World");
-}
 
 fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Todd Crone".to_string())));
@@ -16,9 +25,11 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Tyler Griffin".to_string())));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
     }
 }
 
@@ -33,10 +44,6 @@ fn update_people(mut query: Query<&mut Name, With<Person>>) {
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, add_people)
-        .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
+        .add_plugins((DefaultPlugins, HelloPlugin))
         .run();
 }
-
-
